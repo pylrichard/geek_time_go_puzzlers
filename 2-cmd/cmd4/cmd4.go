c6f239f7d5783a740e29301ed9cc03347b611111
pylrichard <pylrichard@qq.com>
2019-11-30 11:35:33 +0800

    2-命令源码文件修改

--- 2-cmd/cmd4/cmd4.go
+++ 2-cmd/cmd4/cmd4.go
@@ -1,5 +1,5 @@
 package main
- 
+
 import (
 	"flag"
 	"fmt"
@@ -15,11 +15,12 @@ func init() {
 		两种情况都会在调用flag.Parse()时被触发。“运行时恐慌”是Go程序错误处理方面的概念
 	 */
 	//flag.CommandLine = flag.NewFlagSet("", flag.PanicOnError)
-    /*
+	/*
 		输出与cmd3.go一致，但定制方法更加灵活
 		flag.ExitOnError的含义是告诉命令参数容器，当命令后跟--help或者参数设置的不正确时，打印命令参数使用说明后以状态码2结束当前程序
-     */
-    flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
+		状态码2代表用户错误地使用了命令
+	 */
+	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
 	flag.CommandLine.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
 		flag.PrintDefaults()
